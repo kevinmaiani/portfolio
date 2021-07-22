352d148d9466b72155d7bbd94e3f8d0968850e22
Keevvo <kevin.maiani5k@gmail.com>
2021-07-22 21:45:01 +0200

    portfolio v.1

---
+++ index.js
@@ -0,0 +1,29 @@
+// function mouselog(event) {
+//     if(screen.width >600)
+//         document.querySelector('#content').classList.toggle('remove-visible');
+//   }
+$(document).ready(function(){
+    $('.header').height($(window).height());
+})
+
+  // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
+  });
