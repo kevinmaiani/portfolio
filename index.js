bf2943ec8015e7a3fdf1e720ddfcac18e678853e
Keevvo <kevin.maiani5k@gmail.com>
2021-08-05 21:03:09 +0200

    Google Analytics

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ $(window).on("load", function () {
 
   $(document).ready(function () {
     particlesJS.load("particles-js", "assets/particles.json", function () {
-      console.log("callback - particles.js config loaded");
     });
   });
 
